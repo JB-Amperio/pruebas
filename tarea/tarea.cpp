9c88c75e870a424d859e2370972ebcdd299c8ede
Jennifer Benavides <jennifer.hachi.benavides@gmail.com>
2011-10-25 20:00:31 -0500

    Tarea while y do while

---
+++ tarea/tarea.cpp
@@ -0,0 +1,33 @@
+// tarea.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream.h"
+#include "stdlib.h"
+
+
+int main(int argc, char* argv[])
+{	
+	int n,y,x;
+	char r;
+
+	do{
+		system("cls");
+		cout<<"Número de cantidades a evaluar\n";
+		cin>>n;
+		y=1;
+		cout<<"Entre "<<n<<" cantidades separadas con enter\n";
+  		while(y<=n){
+			cin>>x;
+			if (x%2==0){
+				cout<<"Es par\n";
+			}else{
+				cout<<"Es impar\n";
+			}
+			y++;
+		}
+		cout<<"Quiere volver a ejecutar el programa?(s o n)";
+		cin>>r;
+	}while(r=='s');
+	return 0;
+}
